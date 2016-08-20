6f706cfb2ea727ec436cde3e118d97216ed96ac9
hungnt <hungnt167@gmail.com>
2016-08-21 02:04:51 +0700

    full build

--- app/code/community/Zx/Pos/Block/Override/Head.php
+++ app/code/community/Zx/Pos/Block/Override/Head.php
@@ -6,7 +6,19 @@
  * Date: 8/21/16
  * Time: 1:12 AM
  */
-class Head
-{
+require_once(Mage::getModuleDir(null, 'Mage_Page') . DS . 'Block' . DS . 'Html' . DS . 'Head.php');
 
+///var/www/html/hungnt/app/code/core/Mage/Page/Block/Html/Head.php
+class Zx_Pos_Block_Override_Head extends Mage_Page_Block_Html_Head
+{
+    /**
+     * Remove All Items from HEAD entity
+     *
+     * @return Mage_Page_Block_Html_Head
+     */
+    public function removeAllItems()
+    {
+        $this->_data['items'] = array();
+        return $this;
+    }
 }
--- js/zx/pos/app.js
+++ js/zx/pos/app.js
@@ -1,3 +1,4 @@
 /**
  * Created by hungnt on 8/21/16.
  */
+angular.module("Pos", []);
